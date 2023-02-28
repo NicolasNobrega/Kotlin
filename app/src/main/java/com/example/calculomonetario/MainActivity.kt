752e055772df00c400ba39e3b7c94de307554ea9
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-02-27 23:53:15 -0300

    conversor de Moedas

---
+++ app/src/main/java/com/example/calculomonetario/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.calculomonetario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var result: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        result = findViewById<TextView>(R.id.txt_result)
+
+        val buttonConverter = findViewById<Button>(R.id.btn_converter)
+
+        buttonConverter.setOnClickListener {
+            result.text = "novo Texto"
+            result.visibility = View.VISIBLE
+        }
+    }
+}

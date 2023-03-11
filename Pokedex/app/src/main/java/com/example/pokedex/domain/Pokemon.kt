e58528c18a4c1c4b4feb32e80a62558bfabeaa5e
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-03-10 23:22:52 -0300

    Fix RecyclerView

--- Pokedex/app/src/main/java/com/example/pokedex/domain/Pokemon.kt
+++ Pokedex/app/src/main/java/com/example/pokedex/domain/Pokemon.kt
@@ -1,8 +1,10 @@
 package com.example.pokedex.domain
 
-data class Pokemon (
+data class Pokemon(
     val imageURL: String,
     val number: Int,
     val name: String,
     val types: List<PokemonType>
-        )
+) {
+    val formattedNumber = number.toString().padStart(3, '0')
+}
--- Pokedex/app/src/main/java/com/example/pokedex/view/PokemonAdapter.kt
+++ Pokedex/app/src/main/java/com/example/pokedex/view/PokemonAdapter.kt
@@ -37,7 +37,7 @@ class PokemonAdapter(
 
             //TODO: Load image with Glide
 
-            tvNumber.text = "N° ${item.number}"
+            tvNumber.text = "N° ${item.formattedNumber}"
             tvName.text = item.name
             tvType1.text = item.types[0].name
 

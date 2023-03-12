3c2d76c98511a1299300a12e1704f6eb741d472a
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-03-11 21:35:20 -0300

    Retrofit Fixed

--- Pokedex/app/src/main/java/com/example/pokedex/api/PokemonService.kt
+++ Pokedex/app/src/main/java/com/example/pokedex/api/PokemonService.kt
@@ -1,9 +1,13 @@
 package com.example.pokedex.api
 
+
+import com.example.pokedex.api.model.PokemonsApiResult
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface PokemonService {
     @GET("pokemon")
-    fun listRepos(@Query("user") user: String?): Call<List<Repo?>?>?
+    fun listRepos(@Query("limit") limit: Int): Call<PokemonsApiResult>
 }

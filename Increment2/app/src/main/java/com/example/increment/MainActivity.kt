78761356bc95d478f779ed56219257116feeb290
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-05-31 23:42:57 -0300

    Fixed Bugs
    Bug where Number didn't update in real time fixed

--- Increment2/app/src/main/java/com/example/increment/MainActivity.kt
+++ Increment2/app/src/main/java/com/example/increment/MainActivity.kt
@@ -1,25 +1,21 @@
 package com.example.increment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import androidx.viewbinding.ViewBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.example.increment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var number = 0
-
     private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = viewBinding.root
         setContentView(view)
 
-        viewBinding.textButton.setOnClickListener{
-            number++
+        viewBinding.button.setOnClickListener{
             viewBinding.title.text = viewBinding.NameText.text
         }
     }
